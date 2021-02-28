85bb55c05cf5ded79a0624b159585d3ba5a0a1c9
Rubem Silva <rubem.edu@outlook.com>
2021-02-28 18:30:51 -0300

    Add project files.

---
+++ Exercicio_Membros_Estáticos/ConversorDeMoeda.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_Membros_Estáticos
+{
+    class ConversorDeMoeda
+    {
+
+        public static double Dolar;
+        public static double Quantidade;
+
+        public static double Conversao()
+        {
+            double iof = 0;
+
+            double porcentagem = 6.0/100.0;
+
+            double valortotal = 0;
+
+            iof = (Dolar * Quantidade)*porcentagem;
+
+            valortotal = (Dolar * Quantidade) + iof;
+
+            return(valortotal);
+        }
+
+    }
+}
---
+++ Exercicio_Membros_Estáticos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio_Membros_Estáticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            ConversorDeMoeda.Dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            ConversorDeMoeda.Quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Valor a ser pago em reais = "+ConversorDeMoeda.Conversao().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
